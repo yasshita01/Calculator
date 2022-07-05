2c7d424ab576b6da48ad44c4c57ea9958095a15a
celinejp <101208024+celinejp@users.noreply.github.com>
2022-07-05 16:31:27 +0530

    Screen

---
+++ react-calculator/src/components/Screen.js
@@ -0,0 +1,12 @@
+import { useContext } from "react"
+import { CalcContext } from "../context/CalcContext"
+
+const Screen = () => {
+  const { calc } = useContext(CalcContext);
+
+  return (
+    <div className="screen" max={70}  mode="single">{calc.num ? calc.num : calc.res}</div>
+  )
+}
+
+export default Screen
